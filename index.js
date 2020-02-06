9a433d8811ecdf8cd3f29f91fa59e64e98d22931
LupusDiamond <godzplayer@hotmail.com>
2020-02-06 17:17:57 +0200

    reformatted the code a bit

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
-const express = require("express");
-const socket = require("socket.io");
+/// Core file for the Server
 
 // Server setup
+const express = require("express");
 const app = express();
-
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Static files
 app.use(express.static("public"));
 
-// Socket setup
-const io = socket(server);
-
+// This array contains every active user
 let allClients = [];
 
-io.on("connection", socket => {
-  console.log("Made a new connection!");
+// Export things that need to be used by other scripts
 
-  allClients.push(socket);
+module.exports = { server, allClients };
 
-  // Update player count
-  io.sockets.emit("playerCount", allClients.length);
-
-  socket.on("disconnect", () => {
-    console.log("Player disconnected!");
-
-    const i = allClients.indexOf(socket);
-    allClients.splice(i, 1);
-
-    // Update player count
-    io.sockets.emit("playerCount", allClients.length);
-  });
-});
+// Run the following scripts
+require("./server");
--- server.js
+++ server.js
@@ -1,84 +1,91 @@
-//server setup
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
+/// Core file for Web Sockets (Socket.io)
 
-//socket setup
-const io = require('socket.io')(server);
+const { server, allClients } = require("./index.js");
 
-app.use(express.static('public'));
+console.log("hello!");
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+const socket = require("socket.io");
+const io = socket(server);
+
+// Handle new player connections / disconnections
+io.on("connection", socket => {
+  console.log("Made a new connection!");
+
+  // Push the new connection in the array
+  allClients.push(socket);
+
+  // Update player count
+  io.sockets.emit("playerCount", allClients.length);
+
+  socket.on("disconnect", () => {
+    console.log("Player disconnected!");
+
+    // Remo the new connection from the array
+    const i = allClients.indexOf(socket);
+    allClients.splice(i, 1);
+
+    // Update player count
+    io.sockets.emit("playerCount", allClients.length);
+  });
 });
 
-var players = {},
-    unmatched;
+/*var players = {},
+  unmatched;
 
 function joinGame(socket) {
+  players[socket.id] = {
+    opponent: unmatched,
 
+    symbol: "X",
 
-    players[socket.id] = {
+    socket: socket
+  };
 
-
-        opponent: unmatched,
-
-
-        symbol: 'X',
-
-
-        socket: socket
-    };
-
-
-    if (unmatched) {
-        players[socket.id].symbol = 'O';
-        players[unmatched].opponent = socket.id;
-        unmatched = null;
-    } else {
-        unmatched = socket.id;
-    }
+  if (unmatched) {
+    players[socket.id].symbol = "O";
+    players[unmatched].opponent = socket.id;
+    unmatched = null;
+  } else {
+    unmatched = socket.id;
+  }
 }
 
 // Returns the opponent socket
 function getOpponent(socket) {
-    if (!players[socket.id].opponent) {
-        return;
-    }
-    return players[
-        players[socket.id].opponent
-    ].socket;
+  if (!players[socket.id].opponent) {
+    return;
+  }
+  return players[players[socket.id].opponent].socket;
 }
 
-io.on('connection', function (socket) {
-    console.log("Connection established...", socket.id);
-    joinGame(socket);
+io.on("connection", function(socket) {
+  console.log("Connection established...", socket.id);
+  joinGame(socket);
 
+  if (getOpponent(socket)) {
+    socket.emit("game.begin", {
+      symbol: players[socket.id].symbol
+    });
+    getOpponent(socket).emit("game.begin", {
+      symbol: players[getOpponent(socket).id].symbol
+    });
+  }
 
-    if (getOpponent(socket)) {
-        socket.emit('game.begin', {
-            symbol: players[socket.id].symbol
-        });
-        getOpponent(socket).emit('game.begin', {
-            symbol: players[getOpponent(socket).id].symbol
-        });
+  socket.on("make.move", function(data) {
+    if (!getOpponent(socket)) {
+      return;
     }
+    console.log("Move made by : ", data);
+    socket.emit("move.made", data);
+    getOpponent(socket).emit("move.made", data);
+  });
 
-
-    socket.on('make.move', function (data) {
-        if (!getOpponent(socket)) {
-            return;
-        }
-        console.log("Move made by : ", data);
-        socket.emit('move.made', data);
-        getOpponent(socket).emit('move.made', data);
-    });
-
-    // Emit an event to the opponent when the player leaves
-    socket.on('disconnect', function () {
-        if (getOpponent(socket)) {
-            getOpponent(socket).emit('opponent.left');
-        }
-    });
+  // Emit an event to the opponent when the player leaves
+  socket.on("disconnect", function() {
+    if (getOpponent(socket)) {
+      getOpponent(socket).emit("opponent.left");
+    }
+  });
 });
-server.listen(3000);
+
+export { io }*/
