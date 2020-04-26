430b50070b7f5feebf47fb050123b428bfefb49f
Eclipse <godzplayer@hotmail.com>
2020-04-26 17:06:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // Server setup
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
